6923be5a3336e7cb6c74f34e4e441c966cafed89
Sandeepa Silva <silvasandeepaiit@gmail.com>
2026-03-18 22:54:27 +0530

    Revert " Add password visibility toggle to auth forms"

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -30,7 +30,6 @@ export class LoginComponent implements AfterViewInit, OnInit {
     signupCountry = '';
     signupPhone = '';
     signupPassword = '';
-    showSignupPassword = false;
     signupConfirmPassword = '';
     showSignupPassword = false;
     showSignupConfirmPassword = false;
